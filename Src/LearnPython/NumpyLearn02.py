d1d91c7d4b2ca163da3ff1f9ca23fbeb27cb2752
thejhacker <960924584@qq.com>
2018-11-26 15:58:02 +0800

    Update LearnPython

---
+++ Src/LearnPython/NumpyLearn02.py
@@ -0,0 +1,168 @@
+# Datatypes
+# Every numpy array is a grid of elements of the same type. Numpy provides a large set of numeric datatypes that you can
+# use to construct arrays. Numpy tries to guess a datatype when you create an array, but functions that construct arrays
+# usually also include an optional argument to explicitly specify the datatype. Here is an example:
+
+import numpy as np
+
+x = np.array([1, 2])   # Let numpy choose the datatype
+print(x.dtype)         # Prints "int64"
+
+x = np.array([1.0, 2.0])   # Let numpy choose the datatype
+print(x.dtype)             # Prints "float64"
+
+x = np.array([1, 2], dtype=np.int64)   # Force a particular datatype
+print(x.dtype)                         # Prints "int64"
+
+
+print('\n')
+# Array math
+# Basic mathematical functions operate elementwise on arrays, and are available both as operator overloads and as
+# functions in the numpy module:
+
+
+x = np.array([[1,2],[3,4]], dtype=np.float64)
+y = np.array([[5,6],[7,8]], dtype=np.float64)
+
+# Elementwise sum; both produce the array
+# [[ 6.0  8.0]
+#  [10.0 12.0]]
+print(x + y)
+print(np.add(x, y))
+
+# Elementwise difference; both produce the array
+# [[-4.0 -4.0]
+#  [-4.0 -4.0]]
+print(x - y)
+print(np.subtract(x, y))
+
+# Elementwise product; both produce the array
+# [[ 5.0 12.0]
+#  [21.0 32.0]]
+print(x * y)
+print(np.multiply(x, y))
+
+# Elementwise division; both produce the array
+# [[ 0.2         0.33333333]
+#  [ 0.42857143  0.5       ]]
+print(x / y)
+print(np.divide(x, y))
+
+# Elementwise square root; produces the array
+# [[ 1.          1.41421356]
+#  [ 1.73205081  2.        ]]
+print(np.sqrt(x))
+
+
+
+x = np.array([[1,2],[3,4]])
+y = np.array([[5,6],[7,8]])
+
+v = np.array([9,10])
+w = np.array([11, 12])
+
+# Inner product of vectors; both produce 219
+print(v.dot(w))
+print(np.dot(v, w))
+
+# Matrix / vector product; both produce the rank 1 array [29 67]
+print(x.dot(v))
+print(np.dot(x, v))
+
+# Matrix / matrix product; both produce the rank 2 array
+# [[19 22]
+#  [43 50]]
+print(x.dot(y))
+print(np.dot(x, y))
+
+
+
+print('\n')
+x = np.array([[1,2],[3,4]])
+
+print(np.sum(x))  # Compute sum of all elements; prints "10"
+print(np.sum(x, axis=0))  # Compute sum of each column; prints "[4 6]"
+print(np.sum(x, axis=1))  # Compute sum of each row; prints "[3 7]"
+
+x = np.array([[1,2], [3,4]])
+print(x)    # Prints "[[1 2]
+            #          [3 4]]"
+print(x.T)  # Prints "[[1 3]
+            #          [2 4]]"
+
+# Note that taking the transpose of a rank 1 array does nothing:
+v = np.array([1,2,3])
+print(v)    # Prints "[1 2 3]"
+print(v.T)  # Prints "[1 2 3]"
+
+
+print('\n')
+# Broadcasting
+# Broadcasting is a powerful mechanism that allows numpy to work with arrays of different shapes when performing
+# arithmetic operations. Frequently we have a smaller array and a larger array, and we want to use the smaller array
+# multiple times to perform some operation on the larger array.
+#
+# For example, suppose that we want to add a constant vector to each row of a matrix. We could do it like this:
+
+
+# We will add the vector v to each row of the matrix x,
+# storing the result in the matrix y
+x = np.array([[1,2,3], [4,5,6], [7,8,9], [10, 11, 12]])
+v = np.array([1, 0, 1])
+y = np.empty_like(x)   # Create an empty matrix with the same shape as x
+
+# Add the vector v to each row of the matrix x with an explicit loop
+for i in range(4):
+    y[i, :] = x[i, :] + v
+
+# Now y is the following
+# [[ 2  2  4]
+#  [ 5  5  7]
+#  [ 8  8 10]
+#  [11 11 13]]
+print(y)
+
+# This works; however when the matrix x is very large, computing an explicit loop in Python could be slow. Note that
+# adding the vector v to each row of the matrix x is equivalent to forming a matrix vv by stacking multiple copies of v
+# vertically, then performing elementwise summation of x and vv. We could implement this approach like this:
+# We will add the vector v to each row of the matrix x,
+# storing the result in the matrix y
+x = np.array([[1,2,3], [4,5,6], [7,8,9], [10, 11, 12]])
+v = np.array([1, 0, 1])
+vv = np.tile(v, (4, 1))   # Stack 4 copies of v on top of each other
+print(vv)                 # Prints "[[1 0 1]
+                          #          [1 0 1]
+                          #          [1 0 1]
+                          #          [1 0 1]]"
+y = x + vv  # Add x and vv elementwise
+print(y)  # Prints "[[ 2  2  4
+          #          [ 5  5  7]
+          #          [ 8  8 10]
+          #          [11 11 13]]"
+
+
+# Numpy broadcasting allows us to perform this computation without actually creating multiple copies of v. Consider
+# this version, using broadcasting:
+
+
+# We will add the vector v to each row of the matrix x,
+# storing the result in the matrix y
+x = np.array([[1,2,3], [4,5,6], [7,8,9], [10, 11, 12]])
+v = np.array([1, 0, 1])
+y = x + v  # Add v to each row of x using broadcasting
+print(y)  # Prints "[[ 2  2  4]
+          #          [ 5  5  7]
+          #          [ 8  8 10]
+          #          [11 11 13]]"
+
+# The line y = x + v works even though x has shape (4, 3) and v has shape (3,) due to broadcasting; this line works as if
+#     v actually had shape (4, 3), where each row was a copy of v, and the sum was performed elementwise.
+
+
+# Broadcasting two arrays together follows these rules:
+#
+# 1,If the arrays do not have the same rank, prepend the shape of the lower rank array with 1s until both shapes have the same length.
+# 2,The two arrays are said to be compatible in a dimension if they have the same size in the dimension, or if one of the arrays has size 1 in that dimension.
+# 3,The arrays can be broadcast together if they are compatible in all dimensions.
+# 4,After broadcasting, each array behaves as if it had shape equal to the elementwise maximum of shapes of the two input arrays.
+# 5,In any dimension where one array had size 1 and the other array had size greater than 1, the first array behaves as if it were copied along that dimension
